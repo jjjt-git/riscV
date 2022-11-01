d72dd2bc9815c191102fa7a8fb7ab6cdae665f52
Jacob Tilger <jacob.2015@tilger.net>
2022-11-01 13:42:46 +0100

    added null-register

--- register_32bit.vhdl
+++ register_32bit.vhdl
@@ -8,7 +8,12 @@ port (
 );
 end;
 
-architecture with_tmp of register_32bit is
+architecture zero of register_32bit is
+begin
+	content <= "00000000000000000000000000000000";
+end;
+
+architecture def of register_32bit is
 	signal tmp: bit_vector(31 downto 0);
 begin
 	FF: process (set)
