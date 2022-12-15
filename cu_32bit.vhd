db97d9cc97e265f63bf302d53f98c6ebb54b5bf6
Jacob Tilger <jacob.2015@tilger.net>
2022-12-15 17:29:39 +0100

    fixed minor details

--- cu_32bit.vhd
+++ cu_32bit.vhd
@@ -1,10 +1,17 @@
 library ieee;
 
 entity cu_32bit is
+generic (
+	bootloaderStartAddr: bit_vector(31 downto 0) := x"00000000"
+);
 port (
-	memWord: inout bit_vector(31 downto 0);
+	memRd: in bit_vector(31 downto 0);
+	memWr: out bit_vector(31 downto 0);
 	memAddr: out bit_vector(31 downto 0);
+	memLen: out bit_vector(1 downto 0); -- 0: idle, 1: byte, 2: halfword, 3: word
 	fetchInstruction: out bit;
+	write: out bit;
+	read: out bit;
 	clk: in bit;
 	rst: in bit
 );
@@ -44,6 +51,19 @@ architecture def of cu_32bit is
 	-- branch helpers
 	signal pcIB, bAddr: bit_vector(31 downto 0);
 	signal bCond: bit;
+	
+	-- read, write
+	signal memAction: bit_vector(3 downto 0);
+				-- 0001 load byte
+				-- 0101 load byte unsigned
+				-- 0010 load half word
+				-- 0110 load half word unsigned
+				-- 0100 load word
+				-- 1001 write byte
+				-- 1010 write half word
+				-- 1100 write word
+	signal signLoad: bit;
+	signal loadSext: bit_vector(31 downto 0);
 
 	-- alu
 	signal alu0, alu1, aluOut: bit_vector(31 downto 0);
@@ -115,11 +135,18 @@ begin
 	one <= x"FFFFFFFF";
 	zero <= x"00000000";
 
-	SEXTMUX_SIGN : entity work.mux2_32bit port map (
+	SEXTMUX_IMM : entity work.mux2_32bit port map (
 		d0 => zero,
 		d1 => one,
 		sel => msb,
 		o => sextender
+	);
+
+	SEXTMUX_LOAD : entity work.mux2_32bit port map (
+		d0 => zero,
+		d1 => one,
+		sel => signLoad,
+		o => loadSext
 	);
 
 	REGBANK : entity work.register_32bank_32bit port map (
@@ -152,7 +179,7 @@ begin
 		s => pcDtmp,
 		ci => '0'
 	);
-	pcD(31 downto 1) <= pcDtmp(31 downto 1);
+	
 	pcD(0) <= '0';
 
 	BRANCHMUX : entity work.mux2_32bit port map (
@@ -164,26 +191,34 @@ begin
 
 	process (clk)
 	begin
-	if rising_edge(clk) then
+	if clk'event and clk = '1' then
 		if rst = '1' or rstState = "00" then
 			state <= "00001";
-			pcD <= zero; -- start-address here
+			pcD(31 downto 1) <= bootloaderStartAddr(31 downto 1);
 			rstState <= "01";
 			pcSet <= '0';
+			read <= '0';
+			write <= '0';
+			fetchInstruction <= '0';
 		elsif rstState = "01" then
 			rstState <= "10";
 			pcSet <= '1';
 		elsif rstState = "10" then
 			rstState <= "11";
 			pcSet <= '0';
-		elsif state = "00001" then --fetch TODO simul with FIFO-Stack
+		elsif state = "00001" then --fetch
 			state <= "00010";
 			setRegDest <= '0';
 			pcSet <= '0';
 			longJump <= '0';
 
 			fetchInstruction <= '1';
+			read <= '1';
+			memAddr <= pcD;
 		elsif state = "00010" then --decode/execute
+			pcD(31 downto 1) <= pcDtmp(31 downto 1);
+			
+			instruction <= memRd;
 			opcode <= instruction(6 downto 0);
 			rs1 <= instruction(19 downto 15);
 			rs2 <= instruction(24 downto 20);
@@ -347,6 +382,8 @@ begin
 				pcI <= immediate;
 				pcCalt <= cReg0;
 			elsif opcode = C_BRANCH then
+				state <= "01000";
+				
 				pcI <= pcIB;
 				source0 <= rs1;
 				source1 <= rs2;
@@ -392,19 +429,94 @@ begin
 					alu1 <= cReg1;
 					bCond <= not aluOut(0);
 				end if;
-			elsif opcode = C_LOAD then -- TODO after EEI
-			elsif opcode = C_STORE then -- TODO after EEI
+			elsif opcode = C_LOAD then
+				state <= "00100";
+				aluOp <= "0000";
+				source0 <= rs1;
+				
+				immediate(11 downto 0) <= instruction(31 downto 20);
+				immediate(31 downto 12) <= sextender(31 downto 12);
+				
+				alu0 <= immediate;
+				alu1 <= cReg0;
+				memAddr <= aluOut;
+				read <= '1';
+				
+				if funct3 = I_LB then
+					memAction <= "0001";
+					memLen <= "01";
+				elsif funct3 = I_LBU then
+					memAction <= "0101";
+					memLen <= "01";
+				elsif funct3 = I_LH then
+					memAction <= "0010";
+					memLen <= "10";
+				elsif funct3 = I_LHU then
+					memAction <= "0110";
+					memLen <= "10";
+				elsif funct3 = I_LW then
+					memAction <= "0100";
+					memLen <= "11";
+				end if;
+			elsif opcode = C_STORE then
+				state <= "00100";
+				aluOp <= "0000";
+				source0 <= rs1;
+				source1 <= rs2;
+				
+				immediate(11 downto 5) <= instruction(31 downto 25);
+				immediate(4 downto 0) <= rd;
+				immediate(31 downto 12) <= sextender(31 downto 12);
+				
+				alu0 <= immediate;
+				alu1 <= cReg0;
+				memAddr <= aluOut;
+				write <= '1';
+				memWr <= cReg1;
+				
+				if funct3 = I_SB then
+					memAction <= "1001";
+					memLen <= "01";
+				elsif funct3 = I_SH then
+					memAction <= "1010";
+					memLen <= "10";
+				elsif funct3 = I_SW then
+					memAction <= "1100";
+					memLen <= "11";
+				end if;
 			elsif opcode = C_MISC_MEM then -- TODO after EEI
 			elsif opcode = C_SYSTEM then
 				if functlong = I_EBREAK then -- TODO after EEI
 				elsif functlong = I_ECALL then -- TODO after EEI
 				end if;
 			end if;
-		elsif state = "00100" then --execute
+		elsif state = "00100" then --execute write
+			state <= "01000";
+			if memAction(3) = '0' then --read
+				signLoad <= memAction(2) and memRd(31);
+				dest <= rd;
+				if memAction(0) = '1' then
+					dReg(7 downto 0) <= memRd(7 downto 0);
+					dReg(31 downto 8) <= loadSext(31 downto 8);
+				elsif memAction(1) = '1' then
+					dReg(15 downto 0) <= memRd(15 downto 0);
+					dReg(31 downto 16) <= loadSext(31 downto 16);
+				elsif memAction(2) = '1' then
+					dReg <= memRd;
+				end if;
+			else --write
+				dest <= "00000";
+			end if;
 		elsif state = "01000" then --write
 			state <= "00001";
 			setRegDest <= '1';
 			pcSet <= '1';
+			memAddr <= x"00000000";
+			write <= '0';
+			read <= '0';
+			memLen <= "00";
+			memAction <= "0000";
+			memWr <= x"00000000";
 		end if;
 	end if;
 	end process;
