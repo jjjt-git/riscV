308480a76497a10cae3f354dba9194eff1b260ec
Jacob Tilger <jacob.2015@tilger.net>
2022-12-21 08:49:30 +0100

    made cu work

--- cu_32bit.vhd
+++ cu_32bit.vhd
@@ -18,56 +18,61 @@ port (
 end;
 
 architecture def of cu_32bit is
-	signal instruction: bit_vector(31 downto 0);
+	signal instruction, memAddrTmp: bit_vector(31 downto 0);
 
-	signal rs1, rs2, rd, source0, source1, dest: bit_vector(4 downto 0);
+	signal rs1, rs2, rd, source0, source1, dest, destTmp: bit_vector(4 downto 0);
 	signal opcode: bit_vector(6 downto 0);
 	signal funct: bit_vector(9 downto 0);
 	signal funct3: bit_vector(2 downto 0);
 	signal functlong: bit_vector(14 downto 0);
-	signal immediate: bit_vector(31 downto 0);
-	signal one, zero, sextender: bit_vector(31 downto 0);
+
+	signal immediateI, immediateS,
+		immediateB, immediateU,
+		immediateJ, immediate: bit_vector(31 downto 0);
+	signal format: bit_vector(4 downto 0);
+	signal formatI, formatS, formatB, formatU, formatJ: bit;
+
+	signal sextender: bit_vector(31 downto 0);
 	signal msb: bit;
 
 	signal state: bit_vector(4 downto 0);	-- 0 fetch
-						-- 1 decode/execute
-						-- 2 (memory)
+						-- 1 execute
+						-- 2 memory
 						-- 3 write
-						-- 5 jump
-	signal rstState: bit_vector(1 downto 0);	-- 0 PC d
-							-- 1 PC set
-							-- 2 PC confirm
-							-- 3 no RST (normal operation)
+						-- 4 jump
+	signal rstState: bit_vector(1 downto 0);-- 0 PC d
+						-- 1 PC set
+						-- 2 PC confirm
+						-- 3 no RST (normal operation)
+
+	-- control
+	signal crtl_useAluRes, crtl_usePcInAlu,
+		crtl_useImmediateInAlu, crtl_isJmp,
+		crtl_specialPcOp, crtl_writeReg,
+		crtl_negateCond, crtl_memRead,
+		crtl_memWrite, crtl_memUnsigned,
+		crtl_pcRst, crtl_fetch: bit;
+	signal crtl_memLen: bit_vector(1 downto 0);
 
 	-- register-bank
 	signal setRegDest: bit;
-	signal cReg0, cReg1, dReg: bit_vector(31 downto 0);
+	signal cReg0, cReg1, dReg, dRegTmp0, dRegTmp1: bit_vector(31 downto 0);
 
 	-- pc-register
 	signal pcC, pcD: bit_vector(31 downto 0);
-	signal pcSet, longJump: bit;
-	signal pcI0, pcI, pcCalt, pcDtmp: bit_vector(31 downto 0);
+	signal pcSet: bit;
+	signal pcI0, pcI1, pcDtmp, pcDPreRst: bit_vector(31 downto 0);
 
 	-- branch helpers
-	signal pcIB, bAddr: bit_vector(31 downto 0);
 	signal bCond: bit;
 	
 	-- read, write
-	signal memAction: bit_vector(3 downto 0);
-				-- 0001 load byte
-				-- 0101 load byte unsigned
-				-- 0010 load half word
-				-- 0110 load half word unsigned
-				-- 0100 load word
-				-- 1001 write byte
-				-- 1010 write half word
-				-- 1100 write word
 	signal signLoad: bit;
 	signal loadSext: bit_vector(31 downto 0);
 
 	-- alu
 	signal alu0, alu1, aluOut: bit_vector(31 downto 0);
-	signal aluOP: bit_vector(3 downto 0);	-- 0000 add
+	signal aluOp: bit_vector(3 downto 0);	-- 0000 add
 						-- 0001 sub
 						-- 0010 and
 						-- 0011 or
@@ -131,22 +136,15 @@ architecture def of cu_32bit is
 	constant I_AND		: bit_vector(9 downto 0) := "0000000111";
 	constant I_ECALL	: bit_vector(14 downto 0) := "000000000000000";
 	constant I_EBREAK	: bit_vector(14 downto 0) := "000000000001000";
-begin
-	one <= x"FFFFFFFF";
-	zero <= x"00000000";
 
+	constant zero		: bit_vector(31 downto 0) := x"00000000";
+	constant one		: bit_vector(31 downto 0) := x"FFFFFFFF";
+begin
 	SEXTMUX_IMM : entity work.mux2_32bit port map (
 		d0 => zero,
 		d1 => one,
 		sel => msb,
 		o => sextender
-	);
-
-	SEXTMUX_LOAD : entity work.mux2_32bit port map (
-		d0 => zero,
-		d1 => one,
-		sel => signLoad,
-		o => loadSext
 	);
 
 	REGBANK : entity work.register_32bank_32bit port map (
@@ -160,363 +158,386 @@ begin
 		write => dReg
 	);
 
+	ALU : entity work.alu_32bit port map (
+		op0 => alu0,
+		op1 => alu1,
+		res => aluOut,
+		op => aluOp
+	);
+
 	PCREG : entity work.register_32bit port map (
 		data => pcD,
 		content => pcC,
 		set => pcSet
 	);
 
-	PCI0MUX : entity work.mux2_32bit port map (
-		d0 => pcC,
-		d1 => pcCalt,
-		o => pcI0,
-		sel => longJump
-	);
-
 	PCADDER : entity work.adder_32bit(cla) port map (
-		a => pcI0,
-		b => pcI,
+		a => pcC,
+		b => pcI0,
 		s => pcDtmp,
 		ci => '0'
 	);
 	
-	pcD(0) <= '0';
+	--BEGIN DECODE
+	opcode <= instruction(6 downto 0);
 
-	BRANCHMUX : entity work.mux2_32bit port map (
-		d0 => x"00000004",
-		d1 => bAddr,
-		o => pcIB,
-		sel => bCond
-	);
+	rs1 <= instruction(19 downto 15);
+	rs2 <= instruction(24 downto 20);
+	rd <= instruction(11 downto 7);
+
+	funct3 <= instruction(14 downto 12);
+	funct(2 downto 0) <= funct3;
+	funct(9 downto 3) <= instruction(31 downto 25); -- funct7
+	functlong(14 downto 3) <= instruction(31 downto 20); -- func12
+	functlong(2 downto 0) <= funct3;
+
+	msb <= instruction(31);
+
+	immediateI(31 downto 11) <= sextender(31 downto 11);
+	immediateI(10 downto 0) <= instruction(30 downto 20);
+
+	immediateS(31 downto 11) <= sextender(31 downto 11);
+	immediateS(10 downto 5) <= instruction(30 downto 25);
+	immediateS(4 downto 0) <= instruction(11 downto 7);
+
+	immediateB(31 downto 12) <= sextender(31 downto 12);
+	immediateB(11) <= instruction(7);
+	immediateB(10 downto 5) <= instruction(30 downto 25);
+	immediateB(4 downto 1) <= instruction(11 downto 8);
+	immediateB(0) <= zero(0);
+
+	immediateU(31 downto 12) <= instruction(31 downto 12);
+	immediateU(11 downto 0) <= zero(11 downto 0);
+
+	immediateJ(31 downto 20) <= sextender(31 downto 20);
+	immediateJ(19 downto 12) <= instruction(19 downto 12);
+	immediateJ(11) <= instruction(20);
+	immediateJ(10 downto 1) <= instruction(30 downto 21);
+	immediateJ(0) <= zero(0);
+
+	format(0) <= formatI;
+	format(1) <= formatS;
+	format(2) <= formatB;
+	format(3) <= formatU;
+	format(4) <= formatJ;
+
+	with format select immediate <=
+		immediateI when "00001",
+		immediateS when "00010",
+		immediateB when "00100",
+		immediateU when "01000",
+		immediateJ when "10000",
+		x"00000000" when others;
+
+	with opcode select formatI <=
+		'1' when C_OP_IMM,
+		'1' when C_JALR,
+		'1' when C_LOAD,
+		'1' when C_SYSTEM,
+		'0' when others;
+
+	with opcode select formatS <=
+		'1' when C_STORE,
+		'0' when others;
+
+	with opcode select formatB <=
+		'1' when C_BRANCH,
+		'0' when others;
+
+	with opcode select formatU <=
+		'1' when C_LUI,
+		'1' when C_AUIPC,
+		'0' when others;
+
+	with opcode select formatJ <=
+		'1' when C_JAL,
+		'0' when others;
+	-- END DECODE
+
+	source0 <= rs1;
+	source1 <= rs2;
+
+	bCond <= aluOut(0) xor crtl_negateCond;
+
+	with crtl_useAluRes select dReg <=
+		dRegTmp0 when '1',
+		dRegTmp1 when '0';
+
+	with crtl_usePcInAlu select alu0 <=
+		cReg0 when '0',
+		pcC when '1';
+
+	with crtl_useImmediateInAlu select alu1 <=
+		immediate when '1',
+		cReg1 when '0';
+
+	with crtl_pcRst select pcD <=
+		pcDPreRst when '0',
+		bootloaderStartAddr when '1';
+
+	with crtl_isJmp select pcDPreRst <=
+		pcDtmp when '0',
+		aluOut when '1';
+
+	with crtl_isJmp select dRegTmp0 <=
+		aluOut when '0',
+		pcDtmp when '1';
+
+	with crtl_memRead select dRegTmp1 <=
+		immediate when '0',
+		memRd when '1';
+
+	with crtl_specialPcOp and bCond select pcI0 <=
+		x"00000004" when '0',
+		immediate when '1';
+
+	with crtl_writeReg select dest <=
+		rd when '1',
+		"00000" when '0';
+
+	with crtl_fetch select memAddr <=
+		memAddrTmp when '0',
+		pcC when '1';
+
+	with crtl_memRead or crtl_memWrite select memAddrTmp <=
+		x"00000000" when '0',
+		aluOut when '1';
+
+	with crtl_memWrite select memWr <=
+		cReg1 when '1',
+		x"00000000" when '0';
+
+	read <= crtl_memRead;
+	write <= crtl_memWrite;
+	memLen <= crtl_memLen;
+	fetchInstruction <= crtl_fetch;
 
 	process (clk)
 	begin
 	if clk'event and clk = '1' then
 		if rst = '1' or rstState = "00" then
 			state <= "00001";
-			pcD(31 downto 1) <= bootloaderStartAddr(31 downto 1);
 			rstState <= "01";
+
 			pcSet <= '0';
-			read <= '0';
-			write <= '0';
-			fetchInstruction <= '0';
+			crtl_pcRst <= '1';
+			crtl_memRead <= '0';
+			crtl_memWrite <= '0';
+			crtl_fetch <= '0';
 		elsif rstState = "01" then
 			rstState <= "10";
+
 			pcSet <= '1';
 		elsif rstState = "10" then
 			rstState <= "11";
+
 			pcSet <= '0';
+			crtl_pcRst <= '0';
 		elsif state = "00001" then --fetch
 			state <= "00010";
-			setRegDest <= '0';
-			pcSet <= '0';
-			longJump <= '0';
 
-			fetchInstruction <= '1';
-			read <= '1';
-			memAddr <= pcD;
-		elsif state = "00010" then --decode/execute
-			pcD(31 downto 1) <= pcDtmp(31 downto 1);
+			crtl_memRead <= '1';
+			crtl_fetch <= '1';
+			crtl_memLen <= "11";
 			
+			crtl_useAluRes <= '0';
+			crtl_usePcInAlu <= '0';
+			crtl_useImmediateInAlu <= '0';
+			crtl_isJmp <= '0';
+			crtl_specialPcOp <= '0';
+			crtl_writeReg <= '0';
+			crtl_memWrite <= '0';
+
+			pcSet <= '0';
+			setRegDest <= '0';
+		elsif state = "00010" then --decode
+			state <= "00100";
+
 			instruction <= memRd;
-			opcode <= instruction(6 downto 0);
-			rs1 <= instruction(19 downto 15);
-			rs2 <= instruction(24 downto 20);
-			rd <= instruction(11 downto 7);
+			crtl_memRead <= '0';
+			crtl_fetch <= '0';
+			crtl_memLen <= "00";
+			pcSet <= '0';
+		elsif state = "00100" then --execute
+			state <= "01000";
 
-			funct3 <= instruction(14 downto 12);
-			funct(2 downto 0) <= funct3;
-			funct(9 downto 3) <= instruction(31 downto 25); -- funct7
-			functlong(14 downto 3) <= instruction(31 downto 20); -- func12
-			functlong(2 downto 0) <= funct3;
-
-			msb <= instruction(31);
-
-			if opcode = C_OP_IMM then -- I-type
-				immediate(11 downto 0) <= instruction(31 downto 20);
-				immediate(31 downto 12) <= sextender(31 downto 12);
-
-				state <= "00100";
-
-				source0 <= rs1;
-				dest <= rd;
-				dReg <= aluOut;
+			if opcode = C_OP_IMM then
+				crtl_useAluRes <= '1';
+				crtl_usePcInAlu <= '0';
+				crtl_useImmediateInAlu <= '1';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 				if funct3 = I_ADDI then
-					alu0 <= cReg0;
-					alu1 <= immediate;
 					aluOp <= "0000";
 				elsif funct3 = I_SLTI then
-					alu0 <= cReg0;
-					alu1 <= immediate;
 					aluOp <= "1001";
-				elsif funct3 = I_SLTIU then -- TODO
+				elsif funct3 = I_SLTIU then
+					aluOp <= "1011";
 				elsif funct3 = I_ANDI then
-					alu0 <= cReg0;
-					alu1 <= immediate;
 					aluOp <= "0010";
 				elsif funct3 = I_ORI then
-					alu0 <= cReg0;
-					alu1 <= immediate;
 					aluOp <= "0011";
 				elsif funct3 = I_XORI then
-					alu0 <= cReg0;
-					alu1 <= immediate;
 					aluOp <= "0100";
 				elsif funct = I_SLLI then
-					alu0 <= cReg0;
-					alu1(4 downto 0) <= immediate(4 downto 0);
 					aluOp <= "0101";
 				elsif funct = I_SRLI then
-					alu0 <= cReg0;
-					alu1 <= immediate;
 					aluOp <= "0110";
 				elsif funct = I_SRAI then
-					alu0 <= cReg0;
-					alu1(4 downto 0) <= immediate(4 downto 0);
 					aluOp <= "0111";
 				end if;
 			elsif opcode = C_LUI then
-				immediate(31 downto 12) <= instruction(31 downto 12);
-				immediate(12 downto 0) <= zero(12 downto 0);
-				dest <= rd;
-
-				state <= "00100";
-
-				dReg <= immediate;
+				crtl_useAluRes <= '0';
+				crtl_usePcInAlu <= '0';
+				crtl_useImmediateInAlu <= '0';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 			elsif opcode = C_AUIPC then
-				immediate(31 downto 12) <= instruction(31 downto 12);
-				immediate(12 downto 0) <= zero(12 downto 0);
-				dest <= rd;
-
-				state <= "00100";
-
-				alu0 <= immediate;
-				alu1 <= pcC;
-				aluOp <= "0000";
-
-				dReg <= aluOut;
+				crtl_useAluRes <= '1';
+				crtl_usePcInAlu <= '1';
+				crtl_useImmediateInAlu <= '1';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 			elsif opcode = C_OP then
-				source0 <= rs1;
-				source1 <= rs2;
-				dest <= rd;
-				dReg <= aluOut;
-
-				state <= "00100";
-
+				crtl_useAluRes <= '1';
+				crtl_usePcInAlu <= '0';
+				crtl_useImmediateInAlu <= '0';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 				if funct = I_ADD then
 					aluOp <= "0000";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-				elsif funct = I_SUB then
-					aluOp <= "0001";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-				elsif funct = I_AND then
-					aluOp <= "0010";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-				elsif funct = I_OR then
-					aluOp <= "0011";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-				elsif funct = I_XOR then
-					aluOp <= "0100";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
 				elsif funct = I_SLT then
 					aluOp <= "1001";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
 				elsif funct = I_SLTU then
 					aluOp <= "1011";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
+				elsif funct = I_AND then
+					aluOp <= "0010";
+				elsif funct = I_OR then
+					aluOp <= "0011";
+				elsif funct = I_XOR then
+					aluOp <= "0100";
 				elsif funct = I_SLL then
 					aluOp <= "0101";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
 				elsif funct = I_SRL then
 					aluOp <= "0110";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
 				elsif funct = I_SRA then
 					aluOp <= "0111";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
+				elsif funct = I_SUB then
+					aluOp <= "0001";
 				end if;
 			elsif opcode = C_JAL then
-				aluOp <= "0000";
-				dest <= rd;
-				source0 <= rs1;
-
-				immediate(31 downto 20) <= sextender(31 downto 20);
-				immediate(19 downto 12) <= instruction(19 downto 12);
-				immediate(11) <= instruction(11);
-				immediate(10 downto 1) <= instruction (30 downto 21);
-				immediate(0) <= '0';
-
-				state <= "01000";
-
-				alu0 <= pcC;
-				alu1 <= x"00000004";
-				dReg <= aluOut;
-
-				pcI <= immediate;
+				crtl_useAluRes <= '0';
+				crtl_usePcInAlu <= '1';
+				crtl_useImmediateInAlu <= '1';
+				crtl_isJmp <= '1';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 			elsif opcode = C_JALR then
-				aluOp <= "0000";
-				dest <= rd;
-				longJump <= '1';
-
-				immediate(31 downto 20) <= sextender(31 downto 20);
-				immediate(19 downto 12) <= instruction(19 downto 12);
-				immediate(11) <= instruction(11);
-				immediate(10 downto 1) <= instruction (30 downto 21);
-				immediate(0) <= '0';
-
-				state <= "01000";
-
-				alu0 <= pcC;
-				alu1 <= x"00000004";
-				dReg <= aluOut;
-
-				pcI <= immediate;
-				pcCalt <= cReg0;
+				crtl_useAluRes <= '0';
+				crtl_usePcInAlu <= '1';
+				crtl_useImmediateInAlu <= '0';
+				crtl_isJmp <= '1';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 			elsif opcode = C_BRANCH then
-				state <= "01000";
-				
-				pcI <= pcIB;
-				source0 <= rs1;
-				source1 <= rs2;
-
-				immediate(31 downto 12) <= sextender(31 downto 12);
-				immediate(10 downto 5) <= instruction(30 downto 25);
-				immediate(4 downto 1) <= instruction(11 downto 8);
-				immediate(11) <= instruction(7);
-				immediate(0) <= '0';
-
-				bAddr <= immediate;
-
-				-- 1000 EQ
-				-- 1001 LT
-
+				crtl_useAluRes <= '0';
+				crtl_usePcInAlu <= '0';
+				crtl_useImmediateInAlu <= '0';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '1';
+				crtl_writeReg <= '0';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '0';
 				if funct3 = I_BEQ then
+					crtl_negateCond <= '0';
 					aluOp <= "1000";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-					bCond <= aluOut(0);
 				elsif funct3 = I_BNE then
+					crtl_negateCond <= '1';
 					aluOp <= "1000";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-					bCond <= not aluOut(0);
 				elsif funct3 = I_BLT then
+					crtl_negateCond <= '0';
 					aluOp <= "1001";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-					bCond <= aluOut(0);
 				elsif funct3 = I_BLTU then
+					crtl_negateCond <= '0';
 					aluOp <= "1011";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
 				elsif funct3 = I_BGE then
+					crtl_negateCond <= '1';
 					aluOp <= "1001";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-					bCond <= not aluOut(0);
 				elsif funct3 = I_BGEU then
+					crtl_negateCond <= '1';
 					aluOp <= "1011";
-					alu0 <= cReg0;
-					alu1 <= cReg1;
-					bCond <= not aluOut(0);
 				end if;
 			elsif opcode = C_LOAD then
-				state <= "00100";
+				crtl_useAluRes <= '0';
+				crtl_usePcInAlu <= '0';
+				crtl_useImmediateInAlu <= '1';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '1';
+				crtl_memRead <= '1';
+				crtl_memWrite <= '0';
 				aluOp <= "0000";
-				source0 <= rs1;
-				
-				immediate(11 downto 0) <= instruction(31 downto 20);
-				immediate(31 downto 12) <= sextender(31 downto 12);
-				
-				alu0 <= immediate;
-				alu1 <= cReg0;
-				memAddr <= aluOut;
-				read <= '1';
 				
 				if funct3 = I_LB then
-					memAction <= "0001";
-					memLen <= "01";
+					crtl_memLen <= "01";
+					crtl_memUnsigned <= '0';
 				elsif funct3 = I_LBU then
-					memAction <= "0101";
-					memLen <= "01";
+					crtl_memLen <= "01";
+					crtl_memUnsigned <= '1';
 				elsif funct3 = I_LH then
-					memAction <= "0010";
-					memLen <= "10";
+					crtl_memLen <= "10";
+					crtl_memUnsigned <= '0';
 				elsif funct3 = I_LHU then
-					memAction <= "0110";
-					memLen <= "10";
+					crtl_memLen <= "10";
+					crtl_memUnsigned <= '1';
 				elsif funct3 = I_LW then
-					memAction <= "0100";
-					memLen <= "11";
+					crtl_memLen <= "11";
 				end if;
 			elsif opcode = C_STORE then
-				state <= "00100";
+				crtl_useAluRes <= '0';
+				crtl_usePcInAlu <= '0';
+				crtl_useImmediateInAlu <= '1';
+				crtl_isJmp <= '0';
+				crtl_specialPcOp <= '0';
+				crtl_writeReg <= '0';
+				crtl_memRead <= '0';
+				crtl_memWrite <= '1';
 				aluOp <= "0000";
-				source0 <= rs1;
-				source1 <= rs2;
-				
-				immediate(11 downto 5) <= instruction(31 downto 25);
-				immediate(4 downto 0) <= rd;
-				immediate(31 downto 12) <= sextender(31 downto 12);
-				
-				alu0 <= immediate;
-				alu1 <= cReg0;
-				memAddr <= aluOut;
-				write <= '1';
-				memWr <= cReg1;
 				
 				if funct3 = I_SB then
-					memAction <= "1001";
-					memLen <= "01";
+					crtl_memLen <= "01";
 				elsif funct3 = I_SH then
-					memAction <= "1010";
-					memLen <= "10";
+					crtl_memLen <= "10";
 				elsif funct3 = I_SW then
-					memAction <= "1100";
-					memLen <= "11";
-				end if;
-			elsif opcode = C_MISC_MEM then -- TODO after EEI
-			elsif opcode = C_SYSTEM then
-				if functlong = I_EBREAK then -- TODO after EEI
-				elsif functlong = I_ECALL then -- TODO after EEI
+					crtl_memLen <= "11";
 				end if;
 			end if;
-		elsif state = "00100" then --execute write
-			state <= "01000";
-			if memAction(3) = '0' then --read
-				signLoad <= memAction(2) and memRd(31);
-				dest <= rd;
-				if memAction(0) = '1' then
-					dReg(7 downto 0) <= memRd(7 downto 0);
-					dReg(31 downto 8) <= loadSext(31 downto 8);
-				elsif memAction(1) = '1' then
-					dReg(15 downto 0) <= memRd(15 downto 0);
-					dReg(31 downto 16) <= loadSext(31 downto 16);
-				elsif memAction(2) = '1' then
-					dReg <= memRd;
-				end if;
-			else --write
-				dest <= "00000";
-			end if;
-		elsif state = "01000" then --write
+		elsif state = "01000" then --mem
+			state <= "10000";
+
+		elsif state = "10000" then --write
 			state <= "00001";
-			setRegDest <= '1';
+
+			aluOp <= "0000";
+
 			pcSet <= '1';
-			memAddr <= x"00000000";
-			write <= '0';
-			read <= '0';
-			memLen <= "00";
-			memAction <= "0000";
-			memWr <= x"00000000";
+			setRegDest <= '1';
 		end if;
 	end if;
 	end process;
